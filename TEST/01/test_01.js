89e1b5a850e4928136a5e60eb4c8ca15269df800
ngocanvx <lnan.c3vinhxuan@vinhlong.edu.vn>
2025-07-07 21:50:09 +0700

    first commit

---
+++ TEST/01/test_01.js
@@ -0,0 +1,4 @@
+console.log('Hello, world!');
+alert('Xin chào thế giới!');
+var hoten = prompt('Nhập họ tên của bạn:');
+console.log('Họ tên của bạn là: ' + hoten);
---
+++ boiten/js/boiten.js
@@ -0,0 +1,5 @@
+// Hàm hiển thị thông báo
+function ThongBao(noidung) {
+    alert(noidung);
+}
+
