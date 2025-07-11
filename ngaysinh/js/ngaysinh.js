f89c13eac2dbe967d38053b18831a6aa90ce4317
ngocanvx <lnan.c3vinhxuan@vinhlong.edu.vn>
2025-07-11 09:17:53 +0700

    Cap nhat giai ma ngay sinh

---
+++ ngaysinh/js/ngaysinh.js
@@ -0,0 +1,41 @@
+// Hàm tính số mệnh dựa trên ngày sinh
+// Trả về giá trị số mệnh
+function TinhSoMenh(dateNgaySinh) {
+    var somenh = new Date(dateNgaySinh).getDate();
+    switch (somenh) {
+        case 11:
+        case 22:
+            return somenh;
+        default:
+            while (somenh > 10) {
+                var temp = somenh;
+                somenh = 0;
+                while (temp > 0) {
+                    somenh += temp % 10;
+                    temp = Math.floor(temp / 10);
+                }
+            }
+            return somenh;
+    }
+}
+
+// Hàm thực hiện giải mã số mệnh dựa trên ngày sinh
+// Trả về thông điệp dựa trên số mệnh
+function GiaiMaSoMenh(somenh) {
+    fetch('./js/ngaysinh.json')
+        .then(response => response.json())
+        .then(data => {
+            var ketqua = data.find(item => parseInt(item.so) === somenh);
+            if (ketqua) {
+                const content = `<h2>Số mệnh của bạn: ${somenh}</h2> \n` +
+                    `${ketqua.tomtat}\n${ketqua.ynghia}`;
+
+                document.getElementById('modal-text').innerHTML = content;
+                document.getElementById('modal').style.display = 'block';
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching somenh data:', error);
+            alert("Đã xảy ra lỗi khi lấy thông tin số mệnh.");
+        });
+}
